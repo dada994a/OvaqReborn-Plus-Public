dd46b03e16f42c36b082c460df47fa8ad54edf91
ハイピの廃人 <dada994a@users.noreply.github.com>
2025-03-16 19:15:29 +0900

    phase修正

--- src/main/java/net/shoreline/client/util/changelogs/ChangeLog.java
+++ src/main/java/net/shoreline/client/util/changelogs/ChangeLog.java
@@ -13,6 +13,7 @@ public class ChangeLog {
     static {
         changeLogs.add(new ChangeLogEntry(ChangeType.ADD, "UpdateCheckerを追加"));
         changeLogs.add(new ChangeLogEntry(ChangeType.IMPROVE, "IRCに[VIP]などの表示を追加"));
+        changeLogs.add(new ChangeLogEntry(ChangeType.FIX,"Phase修正"));
     }
 
     public static List<ChangeLogEntry> getChangeLogs() {
